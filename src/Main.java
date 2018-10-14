16c8b9f6f7f3978b33b5fb6eac09d423e0d7563e
bizLaptop <kvabrat@gmail.com>
2018-10-14 14:54:44 +0300

    added bigrams

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -18,198 +20,123 @@ public class Main {
 //
 //        File file2 = new File("lt1.txt");
 //        String vnm = new String(Files.readAllBytes(Paths.get(file.getPath())));
-
-//        Scanner myText2 = new Scanner(new File("text.txt"));
+//
+        Scanner myText2 = new Scanner(new File("text.txt"));
 //        (new CryptDecrypt(myText2)).showFrequency();
         String srcText = "вчера";
-        System.out.println(srcText);
-        String newText = new CryptDecrypt(srcText).crypt(2);
-        System.out.println(newText);
-        System.out.println(new CryptDecrypt(newText).decrypt(2));
+//        System.out.println(srcText);
+//        String newText = new CryptDecrypt(srcText).crypt(2);
+//        System.out.println(newText);
+//        System.out.println(new CryptDecrypt(newText).decrypt(2));
+
+        new CryptDecrypt(srcText).freq2();
     }
-    }
+}
 
 class CryptDecrypt {
-    private char[] textArr;
+    //    private char[] textArr;
+    private List<char[]> textList = new ArrayList<>();
     private final char[] alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".toCharArray();
-    private int firstL = alphabet[0];
-    private final int size = alphabet.length;
-    private int countAL[] = new int[size];
-    private double countTL;
+    private final int alphSize = alphabet.length;
+    private final int countAlph[] = new int[alphSize];
 
     CryptDecrypt(Scanner file) {
         while (file.hasNextLine()) {
-            textArr = file.nextLine().toCharArray();
-            countTL += textArr.length;
+            String string = file.nextLine().toLowerCase();
+            textList.add(string.toCharArray());
         }
     }
 
     CryptDecrypt(String text) {
-        textArr = text.toCharArray();
+        textList.add(text.toCharArray());
     }
 
-    private int indexL (char letter) {
-        int if_e2 = (letter == 'ё' ? letter - ('ё'-'е') : letter);
-        int if_after_e = (letter > 'е'? 1 : 0);
-        return (if_e2 + if_after_e) - firstL;
-    }
+    List<char[]> crypt(int shift) {
+        final int firstLetter = alphabet[0];
+        for (char[] string : textList) {
+            for (int i = 0; i < string.length; i++) {
+                int if_e2 = (string[i] == 'ё' ? string[i] - ('ё' - 'е') : string[i]);
+                int if_after_e = (string[i] > 'е' ? 1 : 0);
+                int indexL = (if_e2 + if_after_e) - firstLetter;
 
-    String crypt(int shift) {
-        for (int i = 0; i < textArr.length; i++) {
-            int offset = (indexL(textArr[i]) + shift) % size;
-            textArr[i] = alphabet[offset];
+                int offset = (indexL + shift) % alphSize;
+                string[i] = alphabet[offset];
+            }
         }
-        return String.valueOf(textArr);
+        return textList;
     }
 
-    String decrypt(int shift) {
-        for (int i = 0; i < textArr.length; i++) {
-            int offset = (indexL(textArr[i]) - shift) % size;
-            textArr[i] = alphabet[offset];
+    List<char[]> decrypt(int shift) {
+        return crypt(-shift);
+    }
+
+    void freq2() {
+        List<String> bigramList = new ArrayList<>();
+        for (char[] string : textList) {
+            StringBuilder bigram = new StringBuilder();
+            for (int i = 0; i < string.length; i++) {
+                if (string[i] == ' ')
+                    bigram = new StringBuilder();
+                for (int b = 0, e = alphSize - 1; b < alphSize || e > 0; b++, e--) {
+                    if (string[i] == alphabet[b] || string[i] == alphabet[e]) {
+                        bigram.append(string[i]);
+                        if (bigram.length() > 1) {
+                            //StringBuilder в HashMap повторяется, поэтому используется String
+                            String bigramString = bigram.toString();
+                            bigramList.add(bigramString);
+                            bigram = new StringBuilder();
+                            i--;
+                        }
+                        break;
+                    }
+                }
+            }
+            bigram = new StringBuilder();
         }
-        return String.valueOf(textArr);
+        HashMap<String, Integer> bigramMap = new HashMap<>();
+        HashSet<String> bigramKeys = new HashSet<>();
+        for (String bigram : bigramList) {
+            if(!bigramMap.containsKey(bigram)) {
+                bigramMap.put(bigram, 0);
+                bigramKeys.add(bigram);
+            }
+        }
+        for (String bigram : bigramList) {
+            for (int i = 0; i < bigramMap.size(); i++) {
+                if(bigramMap.containsKey(bigram)) {
+                    bigramMap.replace(bigram, bigramMap.get(bigram) + 1);
+                    break;
+                }
+            }
+        }
+        for (String bigramKey : bigramKeys)
+            System.out.println(bigramKey + ": " + bigramMap.get(bigramKey));
     }
 
-//    String crypt() {
-//        for (int i = 0; i < textArr.length; i++) {
-//            for (int j = 0; j < size; j++) {
-//                if (textArr[i] == srcAlphabet[j]) {
-//                    textArr[i] = cryptAlphabet[j];
-//                    break;
-//                }
-//            }
-//        }
-//        System.out.println(srcAlphabet);
-//        System.out.println(cryptAlphabet);
-//        return String.valueOf(textArr);
-//    }
-//
-//    String decrypt() {
-//        for (int i = 0; i < textArr.length; i++) {
-//            for (int j = 0; j < size; j++) {
-//                if (textArr[i] == cryptAlphabet[j]) {
-//                    textArr[i] = srcAlphabet[j];
-//                    break;
-//                }
-//            }
-//        }
-//        System.out.println(cryptAlphabet);
-//        System.out.println(srcAlphabet);
-//        return String.valueOf(textArr);
-//    }
-
-    void frequency() {
-        for (char letter : textArr) {
-            switch (letter) {
-                case 'а':
-                    countAL[0]++;
-                    break;
-                case 'б':
-                    countAL[1]++;
-                    break;
-                case 'в':
-                    countAL[2]++;
-                    break;
-                case 'г':
-                    countAL[3]++;
-                    break;
-                case 'д':
-                    countAL[4]++;
-                    break;
-                case 'е':
-                    countAL[5]++;
-                    break;
-                case 'ё':
-                    countAL[6]++;
-                    break;
-                case 'ж':
-                    countAL[7]++;
-                    break;
-                case 'з':
-                    countAL[8]++;
-                    break;
-                case 'и':
-                    countAL[9]++;
-                    break;
-                case 'й':
-                    countAL[10]++;
-                    break;
-                case 'к':
-                    countAL[11]++;
-                    break;
-                case 'л':
-                    countAL[12]++;
-                    break;
-                case 'м':
-                    countAL[13]++;
-                    break;
-                case 'н':
-                    countAL[14]++;
-                    break;
-                case 'о':
-                    countAL[15]++;
-                    break;
-                case 'п':
-                    countAL[16]++;
-                    break;
-                case 'р':
-                    countAL[17]++;
-                    break;
-                case 'с':
-                    countAL[18]++;
-                    break;
-                case 'т':
-                    countAL[19]++;
-                    break;
-                case 'у':
-                    countAL[20]++;
-                    break;
-                case 'ф':
-                    countAL[21]++;
-                    break;
-                case 'х':
-                    countAL[22]++;
-                    break;
-                case 'ц':
-                    countAL[23]++;
-                    break;
-                case 'ч':
-                    countAL[24]++;
-                    break;
-                case 'ш':
-                    countAL[25]++;
-                    break;
-                case 'щ':
-                    countAL[26]++;
-                    break;
-                case 'ъ':
-                    countAL[27]++;
-                    break;
-                case 'ы':
-                    countAL[28]++;
-                    break;
-                case 'ь':
-                    countAL[29]++;
-                    break;
-                case 'э':
-                    countAL[30]++;
-                    break;
-                case 'ю':
-                    countAL[31]++;
-                    break;
-                case 'я':
-                    countAL[32]++;
-                    break;
+    void freq() {
+        int countRus = 0;
+        for (char[] string : textList) {
+            for (char letter : string) {
+                for (int b = 0, e = alphSize - 1; b < alphSize || e > 0; b++, e--) {
+                    if (letter == alphabet[b]) {
+                        countRus++;
+                        countAlph[b]++;
+                        break;
+                    } else if(letter == alphabet[e]) {
+                        countRus++;
+                        countAlph[e]++;
+                        break;
+                    }
+                }
             }
         }
 
-    }
 
-//    void showFrequency() {
-//        double freqL;
-//        for (int i = 0; i < size; i++) {
-//            freqL = (countAL[i]/countTL)*100;
-//            System.out.println(srcAlphabet[i] + ": " + freqL);
-//        }
-//    }
+        System.out.println("Количество русских букв в тексте: " + countRus);
+        for (int i = 0; i < alphSize; i++) {
+            double freq = 100.0 * countAlph[i] / countRus;
+            String formattedFreq = new DecimalFormat("#0.00").format(freq);
+            System.out.println(alphabet[i] + ": " + formattedFreq + " %");
+        }
+    }
 }
